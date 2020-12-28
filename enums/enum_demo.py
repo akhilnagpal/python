f77c3244b0759a231b6f867ad2f7cd2eed5a86de
Akhil <akhil_n@yahoo.com>
2020-12-28 20:46:54 +0000

    protobuf

---
+++ enums/enum_demo.py
@@ -0,0 +1,8 @@
+import enums.enum_example_pb2 as enum_example_pb2
+
+enum_message = enum_example_pb2.EnumMessage()
+
+enum_message.id = 232
+enum_message.day_of_week = enum_message.THURSDAY
+
+print(enum_message)
